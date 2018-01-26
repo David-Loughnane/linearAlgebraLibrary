18de7ec555c0b83eaf0db4bc34626d3b20b5069a
David-Loughnane <david.loughnane1@gmail.com>
2018-01-26 10:15:34 +0000

    line equality, intersections, parallel test

--- line.py
+++ line.py
@@ -23,6 +23,52 @@ class Line(object):
 
         self.set_basepoint()
 
+    def __eq__(self, ell):
+        if self.normal_vector.is_zero():
+            if not ell.normal_vector.is_zero():
+                return False
+            else:
+                diff = self.constant_term - ell.constant_term
+                return MyDecimal(diff).is_near_zero()
+        elif ell.normal_vector.is_zero():
+            return False
+
+        if not self.is_parallel_to(ell):
+            return False
+
+        x0 = self.basepoint
+        y0 = ell.basepoint
+        basepoint_difference = x0 - y0
+
+        n = self.normal_vector
+        return basepoint_difference.is_orthogonal_to(n)
+
+    def intersection_with(self, ell):
+        try:
+            A, B = self.normal_vector.coordinates
+            k1 = self.constant_term
+
+            C, D = ell.normal_vector.constant_term
+            k2 = ell.constant_term
+
+            x_numerator = (D * k1) - (B * k2)
+            y_numerator = -(C * k1) + (A * k2)
+            one_over_denom = Decimal('1') / ((A * D) - (B * C))
+
+            return Vector([x_numerator, y_numerator]) * one_over_denom
+
+        except ZeroDivisionError:
+            if self == ell:
+                return self
+            else:
+                return None
+
+    def is_parallel_to(self, ell):
+        n1 = self.normal_vector
+        n2 = ell.normal_vector
+
+        return n1.is_parallel_to(n2)
+
     def set_basepoint(self):
         try:
             n = self.normal_vector
